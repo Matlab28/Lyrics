34a07b684ece3b99d35be41bc0c79040f70d7db7
Matlab Abbaszada <148482779+Matlab28@users.noreply.github.com>
2024-06-02 18:09:17 +0400

    Update TelegramClient.java

--- src/main/java/com/example/lyrics/client/TelegramClient.java
+++ src/main/java/com/example/lyrics/client/TelegramClient.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "telegramApi", url = "https://api.telegram.org/botYOUR_BOT_API")
+@FeignClient(name = "telegramApi", url = "https://api.telegram.org/botYOUR_BOT_TOKEN")
 public interface TelegramClient {
     @GetMapping("/getUpdates?offset={value}")
     RootRequestDto getUpdates(@PathVariable Long value);
